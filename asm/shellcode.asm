db1675f40448445ab00efe39b7ede878830c9408
sloweax <sloweax@users.noreply.github.com>
2023-11-22 18:51:58 -0300

    add syscall_macros.asm

--- asm/shellcode.asm
+++ asm/shellcode.asm
@@ -1,35 +1,7 @@
 ; nasm shellcode.asm
 ; nc pwnable.kr 9026 < shellcode
 
-%macro EXIT 1
-        mov rdi, %1
-        mov rax, 60
-        syscall
-%endmacro
-
-%macro WRITE 3
-        mov rdi, %1
-        mov rsi, %2
-        mov rdx, %3
-        mov rax, 1
-        syscall
-%endmacro
-
-%macro READ 3
-        mov rdi, %1
-        mov rsi, %2
-        mov rdx, %3
-        mov rax, 0
-        syscall
-%endmacro
-
-%macro OPEN 3
-        mov rdi, %1
-        mov rsi, %2
-        mov rdx, %3
-        mov rax, 2
-        syscall
-%endmacro
+%include "../syscall_macros.asm"
 
 [BITS 64]
 
